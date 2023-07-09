a311df42948accddc43c320a163c473d9481ad7a
ruriazz <azizruri.s@gmail.com>
2023-07-09 22:32:01 +0700

    make fully static

--- container/build.sh
+++
@@ -1,8 +0,0 @@
-docker login ghcr.io
-docker buildx create --name Cw4F9_mE47s
-docker buildx use Cw4F9_mE47s
-docker buildx build \
-    --platform linux/amd64 \
-    -t ghcr.io/ruriazz/profile:latest \
-    -f container/Dockerfile \
-    --push .
